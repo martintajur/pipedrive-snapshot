67b62dcac21c626d386174455e8c78c9f71a895e
Martin Tajur <martin@tajur.ee>
2014-09-12 16:08:38 +0300

    add comments to the code (kind of, a lot)

--- download.js
+++ download.js
@@ -1,3 +1,8 @@
+'use strict';
+
+// this script deals with downloading the contents of an entire Pipedrive account into a
+// single datafile in JSON format.
+
 var Pipedrive = require('pipedrive'),
 	apiToken = process.argv[2],
 	targetFile = process.argv[3],
@@ -19,9 +24,13 @@ var pipedrive = new Pipedrive.Client(apiToken, true);
 _.mixin(require('./lib/mixins.js'));
 _.mixin(require('underscore.inflections'));
 
-var downloadedData = { _meta: { start_time: new Date().toISOString() } },
+// bucket for all downloaded data that will get dumped to the JSON file in the end:
+var downloadedData = {
+		_meta: { start_time: new Date().toISOString() }
+	},
 	completedItems = {};
 
+// method that writes the output datafile in JSON format, and then exits:
 var writeFile = function() {
 	var fs = require('fs');
 	downloadedData._meta.end_time = new Date().toISOString();
@@ -30,6 +39,7 @@ var writeFile = function() {
 	process.exit(0);
 }
 
+// make sure we write output gracefully upon early termination:
 process.on('SIGINT', writeFile);
 process.on('SIGTERM', writeFile);
 process.on('SIGHUP', writeFile);
@@ -43,8 +53,11 @@ _.each(objectConfig.objects, function(object) {
 	downloadedData[object] = { total: 0, data: {} };
 
 	var fetchPage = function() {
+
+		// called each time a page of data is fetched:
 		var nextPage = function(data, additionalData) {
-			// more items to download:
+
+			// in case there are more items to download of this kind:
 			if (additionalData && additionalData.pagination && additionalData.pagination.next_start && additionalData.pagination.more_items_in_collection === true && (perObjectTypeLimit == -1 || downloadedData[object].total < perObjectTypeLimit)) {
 				console.log('Downloading ' + object + ' ... ('+downloadedData[object].total+' downloaded)');
 				start = additionalData.pagination.next_start;
@@ -52,17 +65,19 @@ _.each(objectConfig.objects, function(object) {
 				return;
 			}
 
-			// all items of this kind are downloaded:
+			// in case all items of this kind are downloaded:
 			completedItems[object] = true;
 			console.log('All ' + object + ' downloaded. '+downloadedData[object].total+' in total.');
 
-			// all items of all kinds are downloaded:
+			// in case all items of all kinds are downloaded:
 			if (_.keys(completedItems).length === objectConfig.objects.length) {
 				writeFile();
 			}
 		}
 
 		console.log('Fetching ' + object + ' from ' + start + ' to ' + (start+pageSize));
+
+		// fetch all items of kind, one page at a time:
 		pipedrive[_.capitalize(object)].getAll({ start: start, limit: pageSize }, function(err, data, additionalData) {
 			if (err) throw err;
 
@@ -72,6 +87,7 @@ _.each(objectConfig.objects, function(object) {
 				detailedItemsFetched = 0,
 				nextPageCalled = false;
 
+			// called internally to handle calling nextPage():
 			var checkNext = function(data, additionalData) {
 				if (!nextPageCalled && subitemsFetched === subitemsToFetch && detailedItemsToFetch == detailedItemsFetched) {
 					nextPageCalled = true;
@@ -79,6 +95,7 @@ _.each(objectConfig.objects, function(object) {
 				}
 			};
 
+			// iterate through all objects received from the API:
 			_.each(data, function(item) {
 				downloadedData[object].data[item.id] = item.toObject();
 				downloadedData[object].total++;
@@ -87,9 +104,12 @@ _.each(objectConfig.objects, function(object) {
 				if (typeof objectConfig.subObjects[object] !== 'undefined') {
 					downloadedData[object].subitems = downloadedData[object].subitems || {};
 
+					// iterate through all sub-object kinds that need to be fetched for this object kind:
 					_.each(objectConfig.subObjects[object], function(subobject) {
 						subitemsToFetch++;
 						downloadedData[object].subitems[item.id] = downloadedData[object].subitems[item.id] || {};
+
+						// fetch the sub-objects of the current kind (@TODO: pagination support):
 						item['get' + _.capitalize(subobject)](function(subErr, subData, subAdditionalData) {
 							if (subErr) throw subErr;
 							if (!_.isEmpty(subData)) {
@@ -106,6 +126,8 @@ _.each(objectConfig.objects, function(object) {
 				// check if for any object of this kind, detailed data must be fetched via GET /v1/[objectType]/[id]
 				if (objectConfig.detailedObjects.indexOf(object) > -1) {
 					detailedItemsToFetch++;
+
+					// fetch the detailed object from API:
 					pipedrive[_.capitalize(object)].get(item.id, function(detailedErr, detailedItem) {
 						if (detailedErr) throw detailedErr;
 						if (!_.isEmpty(detailedItem)) {
@@ -119,6 +141,7 @@ _.each(objectConfig.objects, function(object) {
 				}
 			});
 
+			// if the object did not have any subobjects, and detailed API request was not needed to download each item separately, we will call nextPage from here directly:
 			if (typeof objectConfig.subObjects[object] === 'undefined' && objectConfig.detailedObjects.indexOf(object) === -1) {
 				nextPage(data, additionalData);
 			}
--- upload.js
+++ upload.js
@@ -1,3 +1,7 @@
+'use strict';
+
+// this scripts uploads the contents of the JSON datafile to a new Pipedrive account
+
 var Pipedrive = require('pipedrive'),
 	apiToken = process.argv[2],
 	sourceFile = process.argv[3],
@@ -16,24 +20,41 @@ if (!sourceFile) {
 	process.exit(2);
 }
 
-var data = require('./' + sourceFile);
-
-var objectConfig = require('./lib/objects.json');
-
-var pipedrive = new Pipedrive.Client(apiToken, true);
+var data = require('./' + sourceFile),
+	idMapper = require('./lib/id-mapper.js')(data),
+	objectConfig = require('./lib/objects.json'),
+	pipedrive = new Pipedrive.Client(apiToken, true);
 
 _.mixin(require('./lib/mixins.js'));
 _.mixin(require('underscore.inflections'));
 
+// iterate through all kinds of objects in the JSON datafile
 _.each(objectConfig.objects, function(object) {
-	_.each(_.values(data[object].data), function(item) {
-		var itemData = transform(object, item);
 
-		if (itemData !== false) {
-			pipedrive[_.capitalize(object)].add(itemData, function(err, createdItem) {
-				if (err) throw new Error("Error: " + err.toString() + " " + JSON.stringify(itemData));
+	// then iterate through all items of one kind
+	_.each(_.values(data[object].data), function(item) {
+
+		// store the old item data with the id-mapper
+		idMapper.setNewId(object, item, false);
+
+		// transform the item to be save'able to the API under new account
+		var transformedItem = transform(object, idMapper.map(object, item));
+
+		// continue only if the item was successfully transformed
+		if (transformedItem !== false) {
+
+			// save the item to the new account via API
+			pipedrive[_.capitalize(object)].add(transformedItem, function(err, createdItem) {
+
+				// and then store the new ID with the old ID
+				idMapper.setNewId(object, item, createdItem);
+
+				if (err) throw new Error("Error: " + err.toString() + " " + JSON.stringify(transformedItem));
 				console.log(_.singularize(_.capitalize(object)) + ' ' + createdItem.id + ' created');
 			});
 		}
+		else {
+			console.log('Ingoring ' + _.singularize(object) + ' (' + JSON.stringify(item.id) + ')');
+		}
 	});
-})
+});
